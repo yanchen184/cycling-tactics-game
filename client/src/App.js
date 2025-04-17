9a7a153ae9c14b7cdd1290fe94867d7479a6c429
yanchen184 <62279165+yanchen184@users.noreply.github.com>
2025-04-17 12:27:26 +0800

    Add App.js component

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>風馳電掣</h1>
+        <h2>一款回合制單車策略對戰遊戲</h2>
+        <p>版本 0.1.0</p>
+        <p>
+          專注於破風戰術和團隊配合
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
